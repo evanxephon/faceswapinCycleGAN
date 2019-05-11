da8babe1a7780e34f4e5a9813a4fdbaada8230cd
evanxephon <1257583883@qq.com>
2019-05-11 21:18:20 +0800

    Create train.py

---
+++ train.py
@@ -0,0 +1,17 @@
+import network
+import data_loader
+
+
+config = {'isTrain': True,
+          'loss_weight_config': {
+          
+          
+          
+          
+          
+          },
+         }
+
+if __name__ == '__main__':
+    dataset = data_loader.
+    model = network.CycleGAN()
