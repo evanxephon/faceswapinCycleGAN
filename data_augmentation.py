93290efabe31e04a62bbdf5864c6e0be8eff552c
evanxephon <1257583883@qq.com>
2019-05-11 13:48:01 +0800

    Create data_augmentation.py

---
+++ data_augmentation.py
@@ -0,0 +1,7 @@
+# code borrowed from https://github.com/shaoanlu/faceswap-GAN/blob/master/data_loader/data_augmentation.py
+import numpy as np
+import cv2
+from umeyama import umeyama
+from scipy import ndimage
+from pathlib import PurePath, Path
+
