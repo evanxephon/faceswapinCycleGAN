7f3ad6928af4c1f97f961c42f0615af5fbedf1fe
evanxephon <1257583883@qq.com>
2019-05-26 18:03:53 +0800

    Update train.py

--- train.py
+++ train.py
@@ -34,6 +34,7 @@ config = {'isTrain': True,
           
          }
 
+torch.set_default_tensor_type(torch.DoubleTensor)
 if __name__ == '__main__':
     
     #build model to calculate perceptual loss 
@@ -48,6 +49,7 @@ if __name__ == '__main__':
     model = network.CycleGAN(vggface_feats, config=config)
     model.train()
     model.cuda()
+    model.double()
     model.initialize_weights()
 
     for epoch in range(config['epochs']):
