e4a2bec1363c6738b6d4ee42c986712d6d0472bd
evanxephon <1257583883@qq.com>
2019-05-26 18:04:38 +0800

    Update train.py

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ import cv2
 import numpy as np
 from PIL import Image
 from torch.utils.data import DataLoader
-
+import torch
 
 config = {'isTrain': True,
           'loss_weight_config': {'reconstruction_loss': 1,
