67325b2285a426aa8db55ae65349ccbdf13a83a3
evanxephon <1257583883@qq.com>
2019-05-30 13:59:10 +0800

    Update loss.py

--- loss.py
+++ loss.py
@@ -8,10 +8,10 @@ def calc_loss(output, target, method='L2'):
     if method == 'L2':
         loss = mse(output, target)
             
-    if method == 'L1':
+    elif method == 'L1':
         loss = torch.sum(torch.abs(output - target))
         
-    if method == 'CE':
+    elif method == 'CE':
         loss = mse(output, target)
     
     return loss
@@ -83,9 +83,9 @@ def perceptual_loss(input_real, fake, vggface, vggface_ft_pl, method='L2',loss_w
     # From MUNIT https://github.com/NVlabs/MUNIT
     PL = 0
     
-    PL += weights[0] * calc_loss(nn.functional.instance_norm(real_ft_l1), nn.functional.instance_norm(fake_ft_l1), 'l1') 
-    PL += weights[1] * calc_loss(nn.functional.instance_norm(real_ft_l2), nn.functional.instance_norm(fake_ft_l2), 'l1')
-    PL += weights[2] * calc_loss(nn.functional.instance_norm(real_ft_l3), nn.functional.instance_norm(fake_ft_l3), 'l1')
-    PL += weights[3] * calc_loss(nn.functional.instance_norm(real_ft_l4), nn.functional.instance_norm(fake_ft_l4), 'l1')
+    PL += weights[0] * calc_loss(nn.functional.instance_norm(real_ft_l1), nn.functional.instance_norm(fake_ft_l1), 'L1') 
+    PL += weights[1] * calc_loss(nn.functional.instance_norm(real_ft_l2), nn.functional.instance_norm(fake_ft_l2), 'L1')
+    PL += weights[2] * calc_loss(nn.functional.instance_norm(real_ft_l3), nn.functional.instance_norm(fake_ft_l3), 'L1')
+    PL += weights[3] * calc_loss(nn.functional.instance_norm(real_ft_l4), nn.functional.instance_norm(fake_ft_l4), 'L1')
     
     return PL
