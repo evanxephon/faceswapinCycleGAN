62623667c45e1fc20e2f867bf61f56bc149b4fef
evanxephon <1257583883@qq.com>
2019-06-05 11:46:15 +0800

    Update train.py

--- train.py
+++ train.py
@@ -58,8 +58,11 @@ if __name__ == '__main__':
    
     vggface, vggface_ft_pl = vggface.resnet50("resnet50_ft_weight.pkl", num_classes=8631)  # Pretrained weights fc layer has 8631 outputs
           
-    
     model = network.CycleGAN(vggface, vggface_ft_pl, config=config)
+    
+    model.train()
+    model.cuda()
+    model.float()
 
 #    model.initialize_weights()
 
@@ -68,24 +71,18 @@ if __name__ == '__main__':
         if epoch % config['save_interval'] == 0:
             model.save_networks(epoch)
                     
-        if epoch > config['cycleepochs']:
+        if epoch >= config['cycleepochs']:
             model.cycle_consistency_loss = True
                     
         for batchnum, batchdata in enumerate(dataloader):
           
-            # need to model.float() every epoch, cuz pytorch reconstruct the graph every epoch
-            
-            model.train()
-            model.cuda()
-            model.float()
-
             model.set_input(batchdata)
             # model.display_train_data(batchdata)
             model.optimize_parameter()
             
             # display reconstruction result
 
-            if batchnum ==  0:
+            if batchnum == 0:
                     
                 print(f'epoch:{epoch} reconstruction result')
               
@@ -122,9 +119,7 @@ if __name__ == '__main__':
             model.display_forward()
             
             print(f'display result epoch: {epoch}')
-            
-            batchsize = config['batchsize']
-            
+                     
             vis.show_swap_result(model.realB.cpu().numpy(), model.displayA.cpu().numpy(),
                                  model.displayAmask.cpu().numpy())
             
