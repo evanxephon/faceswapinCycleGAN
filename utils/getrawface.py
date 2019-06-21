3167fb45ae7b08ca8d5d4b61b7070bf0e4a4654b
evanxephon <1257583883@qq.com>
2019-06-21 18:18:45 +0800

    Update getrawface.py

--- utils/getrawface.py
+++ utils/getrawface.py
@@ -8,12 +8,12 @@ from PIL import Image,ImageOps,ImageDraw
 from umeyama import umeyama
 import numpy as np
 
-def getFaceInVideo(videoPath, svPath, fps=10):
+def getFaceInVideo(videoPath, svPath, eyemaskon=False,fps=10):
     
     if not os.path.isdir(svPath):
         os.mkdir(svPath)
-    if not os.path.isdir(svPath + 'align/'):
-        os.mkdir(svPath + 'align/')
+    if not os.path.isdir(svPath + 'rgb/'):
+        os.mkdir(svPath + 'rgb/')
     
     cap = cv2.VideoCapture(videoPath)
     
@@ -24,49 +24,50 @@ def getFaceInVideo(videoPath, svPath, fps=10):
         if cap.grab():
             
             flag, frame = cap.retrieve()
-                
             numFrame += 1
             if numFrame % fps == 0:
                 
                 # frame matrix is BGR mode, needs to turn it into RGB mode
-
-                frame = BGRtoRGB(frame)  
+                frame = frame[:,:,::-1]
                 frame = Image.fromarray(frame,mode='RGB')
                 
                 bounding_boxes, landmarks = detect_faces(frame)
                 
                 # using the bounding_boxes to cut the face
                 face = frame.crop(bounding_boxes.flatten()[:4])
+                # get eyemask 
+                if eyemaskon:
+                    eyemask = getEyeMask(np.zeros(face.shape[:2]), landmarks[:2,:])
+                    eyemask = eyemask[:,:,None]
                 
                 src_landmarks = get_src_landmarks(bounding_boxes.flatten()[:4], landmarks.flatten())
-                
                 #print(f'srclandmark:{src_landmarks}')
                 
                 tar_landmarks = get_tar_landmarks(face)
                 #print(f'tarlandmark:{tar_landmarks}')
                 
-                align_face = landmarks_match_mtcnn(BGRtoRGB(np.array(face)), src_landmarks, tar_landmarks)  
-#                 print(BGRtoRGB(np.array(face)).shape)
-#                 print(BGRtoRGB(np.array(face)))
+                align_face = landmarks_match_mtcnn(np.array(face)[:,:,::-1], src_landmarks, tar_landmarks)
+                align_mask_eye = landmark_match_mtcnn(eyemask, src_landmarks, tar_landmarks)
                 
                 facePath = svPath + str(numFrame//fps) + ".jpg"
                 face.save(facePath,'JPEG')
                 
-                alignFacePath = svPath + 'align/' + str(numFrame//fps) + ".jpg"
+                alignFacePath = svPath + 'rgb/' + str(numFrame//fps) + ".jpg"
+                alignEyeMaskPath = svPath + 'eyemask/' + str(numFrame//fps) + ".jpg"
                 #align_face.save(alignFacePath,'JPEG')
-                cv2.imwrite(alignFacePath, align_face)
-                
+                cv2.imwrite(alignFacePath, align_face)     
+                cv2.imwrite(alignEyeMaskPath, align_mask_eye)
         else:
             break
             
+# emm, now i know we just use [::-1] to reverse channel           
 def BGRtoRGB(frame):
-    
     for row in range(len(frame)):
         for i in range(len(frame[row])):
-            tunnel = []
-            tunnel.append(frame[row][i][0])
-            tunnel.append(frame[row][i][1])
-            tunnel.append(frame[row][i][2])
+            channel = []
+            channel.append(frame[row][i][0])
+            channel.append(frame[row][i][1])
+            channel.append(frame[row][i][2])
             #print(tunnel)
             frame[row][i][0] = tunnel[2]
             frame[row][i][1] = tunnel[1]
@@ -75,7 +76,6 @@ def BGRtoRGB(frame):
     return frame
             
 def getFaceInImages(imagePath, svpath):
-    
     # traversal the path for the pic
     number = 0
     for filepath, _, files in os.walk(imagePath):
@@ -102,6 +102,7 @@ def get_src_landmarks(bbox, landmarks):
     """    
     src_landmarks = [(int(landmarks[i]-bbox[0]), 
                       int(landmarks[i+5]-bbox[1])) for i in range(5)]
+    
     return src_landmarks
 
 
@@ -134,6 +135,7 @@ def landmarks_match_mtcnn(src_im, src_landmarks, tar_landmarks):
     
     src_tmp = [(int(xy[0]), int(xy[1])) for xy in src_landmarks]
     tar_tmp = [(int(xy[0]), int(xy[1])) for xy in tar_landmarks]
+    
     mat = umeyama(np.array(src_tmp), np.array(tar_tmp), False)[0:2]
     result = cv2.warpAffine(src_im, mat, (src_size[1], src_size[0]), borderMode=cv2.BORDER_REPLICATE) 
     
@@ -141,9 +143,16 @@ def landmarks_match_mtcnn(src_im, src_landmarks, tar_landmarks):
     #print(f'resultsize:{result.shape}')
     return result
 
-
-
+def getEyeMask(face, landmarks):
+    h = face.shape[0]
+    w = face.shape[1]
+    kernel = np.ones([h/12, w/10], np.uint8)
+    face[landmarks[0][0],landmarks[0][1],:] = 255
+    face[landmarks[1][0],landmarks[1][1],:] = 255
+    eyemask = cv2.dilate(face, kernel, iteration=1)
+    return eyemask
+    
 if __name__ == '__main__':
     os.chdir('..')
-    getFaceInVideo('./bush.avi','./faceA/',fps=10)
-    getFaceInVideo('./putin.avi','./faceB/',fps=10)
+    getFaceInVideo('./bush.avi','./faceA/',eyemaskon=False, fps=10)
+    getFaceInVideo('./putin.avi','./faceB/',eyemaskon=False, fps=10)
