b31e9a4b0df526319fbf4562b695746504062110
evanxephon <1257583883@qq.com>
2019-06-25 15:48:16 +0800

    Update geteyemask.py

--- utils/geteyemask.py
+++ utils/geteyemask.py
@@ -14,7 +14,7 @@ def outputmask(imgpath, savepath):
    
         boundingbox, landmarks = detect_faces(imgloaded)
         eyemask = np.zeros(imgloaded.size).astype('uint8')[:,:,None]
-        if landmarks:
+        if isinstance(landmarks, np.ndarray):
             eyeisat = [[int(landmarks.flatten()[i]), int(landmarks.flatten()[i+5])] for i in range(2)]
         #print(eyeisat)
         eyemask = gf.getEyeMask(eyemask, eyeisat)
